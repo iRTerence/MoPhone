55a4286f505e4c91d692c9161088c289283a1559
iRTerence <terencetan@hotmail.ca>
2022-12-19 14:49:03 -0500

    added onclick to change text and button colour

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,17 @@
-console.log('hello');
+// Payment Screen Javascript
+let $topAmount = $('#top-up-amount');
+
+// On click to change the payment colour
+$(document).ready(function() {
+    $(".button-ammount-style").click(function() {
+    //Remove highlight of button click
+     $('.button-ammount').find('*').removeClass("selected-payment")
+      $(this).toggleClass("selected-payment");
+      //change text displayed on top up amount
+      $topAmount.text(`KES ` + this.value);
+    //   $topAmount.val(this.value)
+    //   console.log($topAmount.text())
+    });
+  });
 
 
